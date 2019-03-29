39532cfa5e450a9a99e5561abf7bf2e0a66cf66c
Lucas <lucas123.a17@gmail.com>
2019-03-29 14:53:58 +0100

    Add the matches list

---
+++ app/src/main/java/com/example/mobileproject/DotaHeroMatchesList_RecyclerAdapter.java
@@ -0,0 +1,93 @@
+package com.example.mobileproject;
+
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.mobileproject.model.DotaHero;
+import com.example.mobileproject.model.DotaHeroMatch;
+
+import java.util.List;
+
+
+public class DotaHeroMatchesList_RecyclerAdapter extends RecyclerView.Adapter<DotaHeroMatchesList_RecyclerAdapter.ViewHolder> {
+    private List<DotaHeroMatch> values;
+    private HeroStatActivity activity;
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView txtTitle;
+        public TextView txtName;
+        public TextView txtStat;
+        public View layout;
+
+        public ViewHolder(View v) {
+            super(v);
+            layout = v;
+            txtTitle = v.findViewById(R.id.matcheslist_win);
+            txtName = v.findViewById(R.id.matcheslist_name);
+            txtStat = v.findViewById(R.id.matcheslist_stats);
+        }
+    }
+
+    public void add(int position, DotaHeroMatch item) {
+        values.add(position, item);
+        notifyItemInserted(position);
+    }
+
+    public void remove(int position) {
+        values.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    public DotaHeroMatchesList_RecyclerAdapter(List<DotaHeroMatch> myDataset, HeroStatActivity activity) {
+        values = myDataset;
+        this.activity = activity;
+    }
+
+    @Override
+    public DotaHeroMatchesList_RecyclerAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View v = inflater.inflate(R.layout.matcheslist_layout, parent, false);
+        ViewHolder vh = new ViewHolder(v);
+
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, final int position) {
+        DotaHeroMatch match = values.get(position);
+        /*holder.txtHeader.setText(hero.getLocalized_name());
+        holder.txtFooter.setText(hero.getName());*/
+        holder.txtName.setText(match.getLeague_name());
+        holder.txtStat.setText("K/D/A: " + match.getKills() + "/" + match.getDeaths() + "/" + match.getAssists());
+
+        if ((match.isRadiant() && match.isRadiant_win()) || (!match.isRadiant() && !match.isRadiant_win())) {
+            if (match.isRadiant()) {
+                holder.txtTitle.setText("Victoire du Radiant");
+            } else {
+                holder.txtTitle.setText("Victoire du Dire");
+            }
+            holder.txtTitle.setTextColor(Color.GREEN);
+        }
+        else {
+            if (match.isRadiant()) {
+                holder.txtTitle.setText("Défaite du Radiant");
+            } else {
+                holder.txtTitle.setText("Défaite du Dire");
+            }
+            holder.txtTitle.setTextColor(Color.RED);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return values.size();
+    }
+
+}
--- app/src/main/java/com/example/mobileproject/HeroStatActivity.java
+++ app/src/main/java/com/example/mobileproject/HeroStatActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.TextView;
 
 import com.example.mobileproject.controller.DotaRest;
@@ -43,5 +45,11 @@ public class HeroStatActivity extends AppCompatActivity {
     }
 
     void updateHeroMatches(List<DotaHeroMatch> matches) {
+        RecyclerView recyclerView = findViewById(R.id.matcheslist);
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        RecyclerView.Adapter mAdapter = new DotaHeroMatchesList_RecyclerAdapter(matches, this);
+        recyclerView.setAdapter(mAdapter);
     }
 }
